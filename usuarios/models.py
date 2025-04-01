22e839b88b8642070557a4c38115e40e43cbd2b5
jean <jeanb.santos404@gmail.com>
2025-04-01 17:19:25 -0300

    Feat: CRUD para usuario, com alteração de tipo de conta

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class Usuario(AbstractUser):
     TIPOS_CLIENTE_DICT = {
@@ -16,5 +16,11 @@ class Usuario(AbstractUser):
     tipo_cliente = models.CharField(max_length=20, choices=TIPOS_CLIENTE, default='CLIENTE')
     cpf = models.CharField(max_length=11, unique=True, blank=True, null=True)
     
+    groups = models.ManyToManyField(Group, related_name="user_groups", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="user_permisions", blank=True)
+    
     def __str__(self):
         return f"{self.id} - {self.username}"
+    
+    def is_admin(self):
+        return self.groups.filter(name="ALOCADOR").exists()
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import Group
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+
+from principal.decorators import group_required
+from .models import *
+from .forms import *
+
+
+def addUser(request):
+    if request.method == "POST":
+        form = UsuarioFormSing(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(request.POST.get('password1'))
+            user.save()
+            
+            login(request, user)
+            group, created = Group.objects.get_or_create(name= 'Clientes')
+            
+            user.groups.add(group)
+            return redirect('home')
+        else:
+            return render(request, "user/add.html", {"form" : form, 'titulo': "Criar Usuario"})
+    else:
+        form = UsuarioFormSing()
+        return render(request, "user/add.html", {"form" : form, 'titulo': "Criar Usuario"})
+    
+@login_required
+def editUser(request, id):
+    if not request.user.is_superuser and  id != request.user.id:
+        return redirect ('perfilUser')
+    
+    usuario = Usuario.objects.filter(id = id).first()
+    
+    if request.method == 'POST':
+         form = UsuarioForm(request.POST, request.FILES, instance=usuario)
+         
+         if form.is_valid():
+            user = form.save()
+             
+            if request.user.id == usuario.id:
+                login(request, user)
+            return redirect('perfilUser', id=id)
+    else:
+        form = UsuarioForm(instance=usuario)
+    context = {'form' : form, 'titulo': 'Editar Usuario'}
+    return render(request, 'edit.html', context)
+
+@login_required
+def deleteUser(request, id):
+    usuario = Usuario.objects.filter(id = id).first()
+    if usuario: usuario.delete()
+    return redirect('listUser')
+
+@login_required
+def profile(request, id=None):
+    if not request.user.is_superuser and id  is not None and id != request.user.id:
+        return redirect('profileUser')
+    
+    
+    user=None
+    if id:
+      user = get_object_or_404(Usuario, id=id)
+    else:
+        user = request.user
+    form = UsuarioForm(instance=user)
+
+    return render(request, 'user/profile.html', {'form': form, 'user':user, 'titulo': 'Perfil do Usuario'})
+
+
+@login_required
+def muda_tipo(request, id):
+    if request.method =='POST':
+        tipo = request.POST.get('tipo', "Clientes")
+        user = get_object_or_404(Usuario, id=id)
+        
+        if tipo == "CLIENTE":
+            user.groups.clear()
+            user.is_superuser = False
+            user.tipo_cliente = tipo
+            group, created = Group.objects.get_or_create(name='Clientes')
+            user.groups.add(group)
+            
+        elif tipo == "ALOCADOR":
+            user.groups.clear()
+            user.is_superuser = True
+            user.tipo_cliente = tipo
+            group, created = Group.objects.get_or_create(name='ALOCADOR')
+            user.groups.add(group)
+        
+        user.save()
+        messages.success(request, f"Tipo de usuário alterado com sucesso para {Usuario.TIPOS_CLIENTE_DICT[tipo]}.")
+        
+    return redirect('profileUser', id=id)
+
+@login_required
+def listUser(request):
+    usuarios = Usuario.objects,all()
+    return render(request, 'user/listUser.html', {'usuarios': usuarios})
